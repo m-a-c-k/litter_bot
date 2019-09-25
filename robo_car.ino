ce90c7fc5e6107da7e1b7082de5d573cd2e9c337
mack <mackstarnes@gmail.com>
2019-09-25 16:07:53 -0500

    function'd the motor control

--- robo_car.ino
+++ robo_car.ino
@@ -3,6 +3,8 @@
 //When the code start, it will print the value of the ultrasonic
 //to the serial montor
 #include <UCMotor.h>
+#include <Servo.h>
+
 
 #define TRIG_PIN A2
 #define ECHO_PIN A3
@@ -11,50 +13,60 @@ UC_DCMotor rightMotor1(4, MOTOR34_64KHZ);
 UC_DCMotor leftMotor2(1, MOTOR34_64KHZ);
 UC_DCMotor rightMotor2(2, MOTOR34_64KHZ);
 
+
 void setup()
 {
   Serial.begin(115200);
   pinMode(ECHO_PIN, INPUT); //Set the connection pin output mode Echo pin
   pinMode(TRIG_PIN, OUTPUT);//Set the connection pin output mode trog pin
 }
+
+void fwd() {
+  Serial.println("Forwards");
+  leftMotor1.run(0x02); rightMotor1.run(0x02);
+  leftMotor2.run(0x02); rightMotor2.run(0x02);
+  leftMotor1.setSpeed(100); rightMotor1.setSpeed(100);
+  leftMotor2.setSpeed(100); rightMotor2.setSpeed(100);
+}
+
+void lft() {
+  Serial.println("Backup, Turn");
+  leftMotor1.run(0x01); rightMotor1.run(0x01);
+  leftMotor2.run(0x01); rightMotor2.run(0x01);
+  leftMotor1.setSpeed(100); rightMotor1.setSpeed(100);
+  leftMotor2.setSpeed(100); rightMotor2.setSpeed(100);
+  delay(100);
+  leftMotor1.run(0x03); rightMotor1.run(0x03);
+  leftMotor2.run(0x03); rightMotor2.run(0x03);
+  leftMotor1.setSpeed(200); rightMotor1.setSpeed(200);
+  leftMotor2.setSpeed(200); rightMotor2.setSpeed(200);  
+}
+
+void bck() {
+  Serial.println("Backwards");
+  leftMotor1.run(0x01); rightMotor1.run(0x01);
+  leftMotor2.run(0x01); rightMotor2.run(0x01);
+  leftMotor1.setSpeed(100); rightMotor1.setSpeed(100);
+  leftMotor2.setSpeed(100); rightMotor2.setSpeed(100);
+}
+
 void loop(){
   int temp = readPing();
   Serial.println(temp,DEC);
-  if (temp > 10){
-    //Backward
-        Serial.println("Forwards");
-        leftMotor1.run(0x02); rightMotor1.run(0x02);
-        leftMotor2.run(0x02); rightMotor2.run(0x02);
-        leftMotor1.setSpeed(100); rightMotor1.setSpeed(100);
-        leftMotor2.setSpeed(100); rightMotor2.setSpeed(100);
-        delay(200);
-  }
-  if ( temp < 25) {
-     //left
-        Serial.println("Turn");
-        leftMotor1.run(0x01); rightMotor1.run(0x01);
-        leftMotor2.run(0x01); rightMotor2.run(0x01);
-        leftMotor1.setSpeed(100); rightMotor1.setSpeed(100);
-        leftMotor2.setSpeed(100); rightMotor2.setSpeed(100);
-        delay(100);
-        leftMotor1.run(0x03); rightMotor1.run(0x03);
-        leftMotor2.run(0x03); rightMotor2.run(0x03);
-        leftMotor1.setSpeed(200); rightMotor1.setSpeed(200);
-        leftMotor2.setSpeed(200); rightMotor2.setSpeed(200);
-        delay(400);
-  }
-  if ( temp < 15 ) {  
-    //Forward
-        Serial.println("Backwards");
-        leftMotor1.run(0x01); rightMotor1.run(0x01);
-        leftMotor2.run(0x01); rightMotor2.run(0x01);
-        leftMotor1.setSpeed(100); rightMotor1.setSpeed(100);
-        leftMotor2.setSpeed(100); rightMotor2.setSpeed(100);
-        delay(200);
-  }
+  
+  if (temp > 10)
+    fwd();        
+  
+  if ( temp < 25)
+     lft();  
+  
+  if ( temp < 15 ) 
+      bck();      
+  
   delay(50);
   
   }
+
 
 int readPing()
 {
