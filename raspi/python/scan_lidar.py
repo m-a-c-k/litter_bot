903e166ac298472ddebe5e5927ab7872b42c6a1f
m-a-c-k <mackstarnes@gmail.com>
2020-02-22 10:24:42 -0600

    lidar

--- raspi/python/scan_lidar.py
+++ raspi/python/scan_lidar.py
@@ -1,6 +1,15 @@
 from lidar_lite import Lidar_Lite
 lidar = Lidar_Lite()
 
+def scan_li(c):
+	for i in range(c):
+		distance = lidar.getDistance()
+		print ("Distance to target = ", distance)
+		if (int(distance) < 20):
+        		print ("Too Close!!! Back Off!!")
+
+
+
 connected = lidar.connect(1)
 
 if connected < -1:
@@ -11,11 +20,7 @@ else:
 s = str(input("Start Scanning?y/n : "))
 
 if (s == 'y'):
-	for i in range(1000):
-		distance = lidar.getDistance()
-		print ("Distance to target = ", distance)
-		if (int(distance) < 20):
-        		print ("Too Close!!! Back Off!!")
-if(s == 'n'):
+	scan_li(100)
+if (s == 'n'):
 	print("\nNo scanning then.\n")
 	exit(0)
